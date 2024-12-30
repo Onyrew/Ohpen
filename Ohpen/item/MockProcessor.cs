578c491387a2865c0634fb88c3dab4677faaa18e
bence.wagner <bence.wagner@evosoft.com>
2024-12-30 10:10:05 +0100

    fix mock item processor process time

--- Ohpen/item/MockProcessor.cs
+++ Ohpen/item/MockProcessor.cs
@@ -7,7 +7,7 @@ namespace Ohpen.item
     {
         public async Task<Result> Process(object item)
         {
-            await Task.Delay(new Random().Next(100, 1000));
+            await Task.Delay(500);
             return new Result {
                 Success = true,
                 Message = "N/A"
